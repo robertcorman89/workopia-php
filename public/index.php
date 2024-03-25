db46cc4e40d356a79da33fde7e08bb68021d3365
Robert Corman <robert.corman89@gmail.com>
2024-03-25 16:58:01 +0200

    Database class setup

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 require('../helpers.php');
+require basePath('Database.php');
 require(basePath('Router.php'));
+$config = require basePath('config/db.php');
+$db = new Database($config);
 
 $router = new Router();
 
