1c98c1ae440dc898adb7ca0e3c7d071498fd06be
Robert Corman <robert.corman89@gmail.com>
2024-03-25 17:05:46 +0200

    Query database function

--- public/index.php
+++ public/index.php
@@ -2,12 +2,15 @@
 require('../helpers.php');
 require basePath('Database.php');
 require(basePath('Router.php'));
-$config = require basePath('config/db.php');
-$db = new Database($config);
 
+// Instantiating the router
 $router = new Router();
 
+// Get routes
 $routes = require(basePath('routes.php'));
+// Get current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
+
+// Route the request
 $router->route($uri, $method);
