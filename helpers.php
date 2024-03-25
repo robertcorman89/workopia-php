4c866c41c171699fcd6e2c1a0ba438f59ca9f53a
Robert Corman <robert.corman@tss-yonder.com>
2024-03-25 11:07:46 +0200

    Split UI into Partials

--- helpers.php
+++ helpers.php
@@ -8,3 +8,29 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    if (file_exists($viewPath)) {
+        require($viewPath);
+    } else {
+        echo "View $name not found!";
+    }
+}
+
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+    if (file_exists($partialPath)) {
+        require($partialPath);
+    } else {
+        echo "Partial $name not found";
+    }
+}
