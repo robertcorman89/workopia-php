1e5492d93629c8889c8cb9ac75bd52ec8b2f6be6
Robert Corman <robert.corman89@gmail.com>
2024-03-26 15:51:17 +0200

    Handling Route Params

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -20,7 +20,7 @@ class ListingController
     public function index()
     {
         $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
-        loadView('home', ['listings' => $listings]);
+        loadView('listings/index', ['listings' => $listings]);
     }
     /**
      * Show the create listing form
@@ -36,11 +36,16 @@ class ListingController
      *
      * @return void
      */
-    public function show()
+    public function show($params)
     {
-        $id = $_GET['id'] ?? '';
+        $id = $params['id'] ?? '';
         $params = ['id' => $id];
         $listing = $this->db->query('SELECT * FROM listings WHERE id=:id', $params)->fetch();
+        // Check if listing exists
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
         loadView('listings/show', ['listing' => $listing]);
     }
 }
--- App/views/listings/index.view.php
+++ App/views/listings/index.view.php
@@ -2,7 +2,6 @@
 
 loadPartial('head');
 loadPartial('navbar');
-loadPartial('showcase');
 loadPartial('topbanner');
 ?>
 <!-- Job Listings -->
@@ -33,7 +32,7 @@ loadPartial('topbanner');
                                 <strong>Benefits:</strong> <span><?= $listing->benefits ?></span>
                             </li>
                         </ul>
-                        <a href="/listing?id=<?= $listing->id ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
+                        <a href="/listing/<?= $listing->id ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
                             Details
                         </a>
                     </div>
