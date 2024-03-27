b967a88fdcdb68f1932938efbcb035d88bf27c7e
Robert Corman <robert.corman89@gmail.com>
2024-03-27 10:53:19 +0200

    Delete listing

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -35,6 +35,7 @@ class ListingController
     /**
      * Show a single listing
      *
+     * @param array $params
      * @return void
      */
     public function show($params)
@@ -92,5 +93,26 @@ class ListingController
             // Redirect to listings
             redirect('/listings');
         }
+    }
+
+    /**
+     * Delete a job listing
+     * 
+     * @param array $params
+     * @return void
+     */
+    public function destroy($params)
+    {
+        $id = $params['id'];
+        $params = [
+            'id' => $id
+        ];
+        $listing = $this->db->query('SELECT * FROM listings WHERE id=:id', $params)->fetch();
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+        $this->db->query('DELETE FROM listings WHERE id = :id', $params);
+        redirect('/listings');
     }
 }
--- Framework/Router.php
+++ Framework/Router.php
@@ -82,6 +82,11 @@ class Router
     public function route($uri)
     {
         $requestMethod = $_SERVER['REQUEST_METHOD'];
+        // Check for _method input
+        if ($requestMethod === 'POST' && isset($_POST['_method'])) {
+            // Override the request method with the vlaue of _method
+            $requestMethod = strtoupper($_POST['_method']);
+        }
         foreach ($this->routes as $route) {
 
             // Split the current uri into segments
