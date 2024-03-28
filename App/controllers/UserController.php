1df7bafdeb370b6bd56a23986da38570debf377c
Robert Corman <robert.corman89@gmail.com>
2024-03-28 10:05:27 +0200

    Add Session class and set user

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session;
 
 class UserController
 {
@@ -94,6 +95,15 @@ class UserController
             'password' => password_hash($password, PASSWORD_DEFAULT)
         ];
         $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+        // Get new user id
+        $userId = $this->db->conn->lastInsertId();
+        Session::set($user, [
+            'id' => $userId,
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state
+        ]);
         redirect('/');
     }
 }
